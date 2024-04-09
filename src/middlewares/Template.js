5f5b4f50d000039ad897ea1b160d337abd36c20c
Eugene Nesvetaev <dev@nesvet.dev>
2024-04-09 18:13:19 +0400

    Added support of INSITE_CLIENT_* env vars

--- src/middlewares/Template.js
+++ src/middlewares/Template.js
@@ -1,17 +1,25 @@
+import { isEmpty } from "@nesvet/n";
 import { handleRequestCompressed } from "../compression";
 
+
+const { INSITE_TITLE } = process.env;
+
+const envGlobals = {};
+for (const key in process.env)
+	if (/^INSITE_CLIENT_/.test(key))
+		envGlobals[key.replace(/INSITE_CLIENT_/, "").toLowerCase()] = process.env[key];
 
 const headers = { "Content-Type": "text/html; charset=utf-8" };
 
 
 class InSiteTemplateMiddleware {
-	constructor(options = {}) {
-		const {
-			requestRegExp = /.*/,
-			title = "inSite",
-			css = [],
-			rootId = "app"
-		} = options;
+	constructor({
+		requestRegExp = /.*/,
+		globals = {},
+		title = INSITE_TITLE ?? "inSite",
+		css = [],
+		rootId = "app"
+	} = {}) {
 		
 		this.listeners = { GET: [
 			[ requestRegExp, this.handler ]
@@ -21,15 +29,18 @@ class InSiteTemplateMiddleware {
 		this.css = Array.isArray(css) ? css : [ css ];
 		this.rootId = rootId;
 		
+		Object.assign(globals, envGlobals);
+		
 		this.html =
 			"<!DOCTYPE html>" +
 			"<html lang=\"ru\">" +
 			"<head>" +
 				"<meta charset=\"utf-8\" />" +
 				"<meta name=\"viewport\" content=\"minimum-scale=1, initial-scale=1, width=device-width\" />" +
-				`<title>${this.title}</title>${
-					this.css.map(fileName => `<link rel="stylesheet" href="${fileName}" />`).join("")
-				}<script type="text/javascript" src="/index.js" defer></script>` +
+				`<title>${this.title}</title>` +
+				`${this.css.map(fileName => `<link rel="stylesheet" href="${fileName}" />`).join("")}` +
+				`${isEmpty(globals) ? "" : `<script>globalThis.__insite=${JSON.stringify(globals)};</script>`}` +
+				"<script type=\"text/javascript\" src=\"/index.js\" defer></script>" +
 			"</head>" +
 			"<body>" +
 				`<div id="${this.rootId}">` +
